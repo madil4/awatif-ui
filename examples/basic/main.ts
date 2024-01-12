51609cbd7c89f063d9afc11fbe3797305905fc5a
Mohamed Adil <me@mohamedadil.com>
2024-01-12 20:27:15 +0100

    first app

--- examples/basic/main.ts
+++ examples/basic/main.ts
@@ -1,5 +1,37 @@
-import van from "vanjs-core";
+import { app } from "../../src/main";
 
-const { div } = van.tags;
+const nodes = [
+  [8, 12.5, 0],
+  [15, 12.5, 0],
+  [8, 12.5, 8],
+];
+const elements = [
+  [0, 1],
+  [1, 2],
+];
+const assignments = [
+  {
+    node: 0,
+    support: [true, true, true],
+  },
+  {
+    node: 2,
+    support: [true, true, true],
+  },
+  {
+    node: 1,
+    load: [0, 0, -10],
+  },
+  {
+    element: 0,
+    area: 1.2,
+    elasticity: 200,
+  },
+  {
+    element: 1,
+    area: 1.2,
+    elasticity: 200,
+  },
+];
 
-van.add(document.body, div("basic example"));
+app(nodes, elements, assignments);
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import van from "vanjs-core";
 
 const { div } = van.tags;
 
-van.add(document.body, div("ui lib"));
+export function app(nodes: any, elements: any, assignments: any) {
+  van.add(document.body, div(nodes, elements, assignments));
+}
