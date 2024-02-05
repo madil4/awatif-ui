1ecc9b677d9a29caff28da05a859ac46ebe39de8
Mohamed Adil <me@mohamedadil.com>
2024-02-05 20:08:00 +0100

    fix deformed shape toggle bug

--- src/App.ts
+++ src/App.ts
@@ -30,7 +30,7 @@ function App({ model, parameters, onParameterChange, settings }: AppType) {
     orientations: van.state(settings?.orientations ?? false),
     supports: van.state(settings?.supports ?? true),
     loads: van.state(settings?.loads ?? true),
-    deformedShape: van.state(settings?.loads ?? false),
+    deformedShape: van.state(settings?.deformedShape ?? false),
     elementResults: van.state(settings?.elementResults ?? "none"),
     nodeResults: van.state(settings?.nodeResults ?? "none"),
   };
